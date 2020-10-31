1ebf93320ae855621b0974bed5225ea18966d38d
Mithun Prabhu <69952563+MithunPrabhu777@users.noreply.github.com>
2020-10-31 23:29:23 +0530

    Create Your Own😉

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="container">
+        <p className="content">About Page</p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
