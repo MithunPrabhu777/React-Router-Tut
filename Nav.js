844294b2bc223620fbf6bd27d871b1dafcff91e7
Mithun Prabhu <69952563+MithunPrabhu777@users.noreply.github.com>
2020-11-02 19:00:39 +0530

    Update Nav.js

--- Nav.js
+++ Nav.js
@@ -1,16 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GiBrute } from "react-icons/gi";
 
 function Nav() {
   return (
     <nav>
-      <h1>Logo</h1>
+      <h3>
+        <GiBrute />
+        onster
+      </h3>
       <ul className="nav-links">
+        <Link to="/">
+          <li
+            style={{
+              color: "white",
+            }}
+          >
+            Home
+          </li>
+        </Link>
         <Link to="about">
-          <li style={{ color: "white" }}>About</li>
+          <li
+            style={{
+              color: "white",
+            }}
+          >
+            About
+          </li>
         </Link>
         <Link to="/shop">
           <li style={{ color: "white" }}>Shop</li>
+        </Link>
+        <Link to="/contact">
+          <li style={{ color: "white" }}>Contact</li>
         </Link>
       </ul>
     </nav>
